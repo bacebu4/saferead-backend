294031a4b69144038c963afee6aa863c3378280b
bacebu4 <vasua14735@icloud.com>
2021-02-21 21:28:11 +0300

    refactor: better validation of what type

--- utils/validate.util.js
+++ utils/validate.util.js
@@ -1,37 +1,39 @@
 /* eslint-disable operator-linebreak */
 const { decode } = require("./decode.util");
+const { openNoteTag } = require("./html.util");
 
+/**
+ * @param {*} data - gmail response from getting email
+ * @returns {"ibooks" | "litres" | "empty"} reading service name
+ */
 const validate = (data) => {
   let encodedHtml;
-  if (data.data.payload.parts && data.data.payload.parts.length > 1) {
-    encodedHtml = data.data.payload.parts[1].body.data;
+  const payload = data.data.payload;
+
+  if (payload.parts && payload.parts.length > 1) {
+    encodedHtml = payload.parts[1].body.data;
   }
+
   if (encodedHtml) {
     const html = decode(encodedHtml, "utf-8");
-    const appleTagIndex = Math.max(
-      html.indexOf("Apple Books. <br>"),
-      html.indexOf("Книги. <br>"),
-    );
-    if (appleTagIndex !== -1) {
+
+    const appleTagIndex = html.indexOf(openNoteTag);
+
+    if (appleTagIndex > -1) {
       return "ibooks";
     }
   } else {
-    let attachmentId;
-    let mimeType;
-    if (data.data.payload.parts && data.data.payload.parts.length > 1) {
+    if (payload.parts && payload.parts.length > 1) {
       if (
-        data.data.payload.parts[1].body &&
-        data.data.payload.parts[1].body.attachmentId
+        payload.parts[1].body &&
+        payload.parts[1].body.attachmentId &&
+        payload.parts[1].mimeType === "text/plain"
       ) {
-        attachmentId = data.data.payload.parts[1].body.attachmentId;
-        mimeType = data.data.payload.parts[1].mimeType;
+        return "litres";
       }
     }
-
-    if (attachmentId && mimeType === "text/plain") {
-      return "litres";
-    }
   }
+
   return "empty";
 };
 
