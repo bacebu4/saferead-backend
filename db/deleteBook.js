55cb93ac78277346d0049890e90e583857bab203
bacebu4 <vasua14735@icloud.com>
2021-03-06 12:13:51 +0300

    feat: deleting book properly

--- db/deleteBook.js
+++ db/deleteBook.js
@@ -1,14 +1,42 @@
-/* eslint-disable camelcase */
 const { getConnection } = require("typeorm");
 
-const deleteBook = async (book_id) => {
+const deleteBook = async (bookId) => {
   const manager = await getConnection();
+
   const data = await manager.query(
     /* sql */ `
     delete from books
     where book_id = $1;
   `,
-    [book_id],
+    [bookId],
+  );
+
+  // deleting daily notes that refers to that book
+  await manager.query(
+    /* sql */ `
+    WITH x AS (
+      SELECT
+        dn. "noteId"
+      FROM
+        daily_notes dn
+      LEFT JOIN notes n ON dn. "noteId" = n.note_id
+      WHERE
+        date_part('year',
+          dn.date) = date_part('year',
+          now())
+        AND date_part('month',
+          dn.date) = date_part('month',
+          now())
+        AND date_part('day',
+          dn.date) = date_part('day',
+          now())
+        AND n.book_id = $1
+    ) DELETE FROM daily_notes dn
+    WHERE dn. "noteId" in(
+        SELECT
+          "noteId" FROM x);
+    `,
+    [bookId],
   );
 
   return data;
